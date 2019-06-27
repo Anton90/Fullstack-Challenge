a8014e36ea6a55ed70a38e73092046e4555ee98d
Anton Everaert <antoneveraert@gmail.com>
2019-06-27 11:35:47 +0200

    stupid stuff

--- Server/app.js
+++ Server/app.js
@@ -10,26 +10,12 @@ app.get('/', (req, res) => {
 	res.send("We are on home"); 
 })
 
-app.use((req , res , next) => {
-	res.header('Allow-Control-Allow-Origin' , '*');
-	res.header(
-	'Allow-Control-Allow-Headers' ,
-	'origin , Content-type , Accept , Authentication'
-	);
-	
-	if(req.method == 'OPTIONS'){
-	res.header('Allow-Control-Allow-Headers' , 'PUT , POST , GET , DELETE, PATCH');
-	return res.status(200).json({});
-	}
-	next();
-})
-
 //Import routes
 const issuesRoute = require('./routes/issues');
 const usersRoute = require('./routes/users');
 
-app.use('/api/issues', issuesRoute); 
-app.use('/api/users', usersRoute); 
+app.use('/issues', issuesRoute); 
+app.use('/users', usersRoute); 
  
 
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true },
